dfdcf94c44c7a0f1d8ac9950d6eb3b357f51da0d
Mahmod-SO <mahmodshodipo@gmail.com>
2026-01-22 20:11:23 +0100

    Add files via upload

---
+++ simple_code.R
@@ -0,0 +1,10 @@
+# Variables
+
+name <- "Shodipo Mahmod"
+affiliation <- "Igbinedion University"
+fav_gene <- "SHH"
+organism <- "humans"
+
+#Full sentence with variables
+paste0("Hi, my name is ", name,", ", "a graduate of ", affiliation,". My favourite gene is ", fav_gene, " in ", organism, "." )
+
